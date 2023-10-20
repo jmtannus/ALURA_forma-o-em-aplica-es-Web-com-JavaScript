bbbe7656576625d29ef2c4d7f52f295eb1a370d2
Janaina <jmtannus@gmail.com>
2023-10-19 22:20:54 -0300

    fncao gene

---
+++ aluramidi/desafio/desafioMain.js
@@ -0,0 +1,3 @@
+document.querySelector('input[type=tel]')
+let inputTelefone = document.querySelector('input[type=tel]');
+<input type="tel" placeholder="DIGITE SEU TELEFONE">
--- aluramidi/main.js
+++ aluramidi/main.js
@@ -1,1 +1,27 @@
-document.querySelector('.tecla_pom');
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+
+    listaDeTeclas[contador].onclick =  function () {
+        tocaSom('#som_tecla_pom')}
+    
+    contador = contador + 1;
+
+    console.log(contador);
+}
+
+//para
+/*for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }*/
